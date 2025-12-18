eed01b0f381cf3e5d7df60bebebac62d6241bdc4
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-12-18 16:56:39 +0100

    LawClient module upload. Closes #2

---
+++ LawClient-Service/src/test/java/org/example/lawclientservice/LawClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lawclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawClientServiceApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
