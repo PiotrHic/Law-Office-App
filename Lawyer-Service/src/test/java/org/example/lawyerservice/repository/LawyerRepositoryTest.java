2a1748ec8ca2a3151a63427c88f95ce5c3d012f6
Piotr Zbigniew Hic <piotrhic@gmail.com>
2026-01-17 19:57:46 +0100

    Create Tests for the LawClient Service. Closes #48

--- Lawyer-Service/src/test/java/org/example/lawyerservice/repository/LawyerRepositoryTest.java
+++ Lawyer-Service/src/test/java/org/example/lawyerservice/repository/LawyerRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.UUID;
-
 import org.example.lawyerservice.domain.Lawyer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
